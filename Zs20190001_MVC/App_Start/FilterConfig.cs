3febe5c88a9234f32d16d86e388ec84629c0c109
jj w <zjwjj@outlook.com>
2019-05-16 10:38:01 +0800

    添加项目文件。

---
+++ Zs20190001_MVC/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Zs20190001_MVC
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Zs20190001_MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Zs20190001_MVC.MvcApplication" Language="C#" %>
